7018aa1c5cdd74370cc0f931384fae34e26a346f
Felix <fiymi666@gmail.com>
2020-02-07 21:01:55 +0800

    blockqueue解决方案

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,20 @@
 package com.github.hcsp.multithread;
 
+import com.sun.applet2.AppletParameters;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ProducerConsumer1 {
+    static BlockingQueue<Integer> queue = new LinkedBlockingQueue(1);
+    static BlockingQueue<Integer> newqueue = new LinkedBlockingQueue(1);
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
+
 
         producer.start();
         consumer.start();
@@ -14,11 +25,32 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; ++i) {
+                Integer num = new Random().nextInt();
+                try {
+                    queue.put(num);
+                    System.out.println("Producing " + num);
+                    newqueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; ++i) {
+                try {
+                    Integer num = queue.take();
+                    System.out.println("Consuming " + num);
+                    newqueue.put(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
