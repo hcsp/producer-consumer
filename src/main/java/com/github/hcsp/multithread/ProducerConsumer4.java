83c8d3665b9fabe1237c93278fe06641344fea4e
JN0916 <56193588+JN0916@users.noreply.github.com>
2020-02-10 14:19:52 +0800

    生产者-消费者模型练习 (#101)
    生产者-消费者模型练习 (#101)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,92 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        SharedData shared = new SharedData();
+
+        Thread producer = new Thread(new Producer(shared), "producer");
+        Thread consumer = new Thread(new Consumer(shared), "consumer");
+
+        consumer.start();
+        producer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        private SharedData shared;
+
+        Producer(SharedData shared) {
+            this.shared = shared;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int data = new Random().nextInt();
+                    shared.produce(data);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private SharedData shared;
+
+        Consumer(SharedData shared) {
+            this.shared = shared;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int data = shared.consume();
+                    //use data
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class SharedData {
+        int data = 0;
+        Semaphore semproducer = new Semaphore(1);
+        //set initial count to 0, so a consumer needs to wait until data is available
+        Semaphore semconsumer = new Semaphore(0);
+
+        int consume() throws InterruptedException {
+            try {
+                semconsumer.acquire();
+                System.out.println("Consumed:" + data);
+                Thread.sleep(10);
+                return data;
+            } catch (InterruptedException e) {
+                throw e;
+            } finally {
+                semproducer.release();
+            }
+        }
+
+        void produce(int data) throws InterruptedException {
+            try {
+                semproducer.acquire();
+                this.data = data;
+                System.out.println("Produced:" + data);
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semconsumer.release();
+            }
+        }
     }
 }
