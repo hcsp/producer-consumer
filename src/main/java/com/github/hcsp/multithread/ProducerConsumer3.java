da58c76a81da31b0a71f43d8dbf4d5689864b66d
xuanzebin <38971117+xuanzebin@users.noreply.github.com>
2020-02-16 12:44:50 +0800

    feat: create a producer-consumer model (#103)
    feat: create a producer-consumer model (#103)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,49 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue(1);
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int product = new Random().nextInt();
+                try {
+                    queue.put(product);
+                    System.out.println("Producing " + product);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int product = queue.take();
+                    System.out.println("Consuming " + product);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
