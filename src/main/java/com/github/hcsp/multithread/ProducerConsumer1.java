28f26bd42cb4286d80353ca0aca27c8b490ba7cf
benectic <xuanzebin@126.com>
2020-02-16 12:43:18 +0800

    feat: create a producer-consumer model

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static Optional<Integer> container = Optional.empty();
+    private static Object lock = new Object();
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -14,11 +19,43 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    if (container.isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int product = new Random().nextInt();
+                    container = Optional.of(product);
+                    System.out.println("Producing " + product);
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    if (!container.isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int result = container.get();
+                    System.out.println("Consuming " + result);
+                    container = Optional.empty();
+                    lock.notify();
+                }
+            }
+        }
     }
 }
