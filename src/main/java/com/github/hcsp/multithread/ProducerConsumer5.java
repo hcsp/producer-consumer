815f426b2d47897c4e148a2913bfddca498d7217
YingHuaLiu <35859346+YingHuaLiu@users.noreply.github.com>
2020-02-18 07:35:01 +0800

    用5种方法解决生产者消费者问题 (#104)
    用5种方法解决生产者消费者问题 (#104)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,52 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static Exchanger<Integer> exchanger = new Exchanger<Integer>();
+    static Exchanger<Integer> signal = new Exchanger<Integer>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Integer value = new Random().nextInt();
+                    System.out.println("Producing " + value);
+                    exchanger.exchange(value);
+                    signal.exchange(null);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int value = exchanger.exchange(null);
+                    System.out.println("Consuming " + value);
+                    signal.exchange(value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
