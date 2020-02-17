e00163ea69979eaf160d34f3f605fbfd0e95f7bf
YingHuaLiu <1042413535@qq.com>
2020-02-17 23:15:32 +0800

    用5种方法解决生产者消费者问题

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,70 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static int condition = 0; //判断盘子是否为空，1为不空，0为空
+    public static int apple = 0;  //盘子中存的数
+    private static ReentrantLock lock = new ReentrantLock();
+    private static Condition notConsumedYet = lock.newCondition();
+    private static Condition notProducedYet = lock.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (condition == 1) {
+                        try {
+                            notConsumedYet.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    apple = (int) (1 + Math.random() * 10);
+                    System.out.println("Producing " + apple);
+                    condition = 1;
+                    notProducedYet.signal();
+                }finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (condition == 0) {
+                        try {
+                            notProducedYet.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + apple);
+                    condition = 0;
+                    notConsumedYet.signal();
+                }finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,62 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static final Semaphore full= new Semaphore(0); //初始化0个产品
+    static final Semaphore empty= new Semaphore(1); //初始化有1个空余位置放置产品
+    static final Semaphore mutex = new Semaphore(1); //mutex 初始化每次最多只有一个线程可以读写
+
+    public static int apple;  //盘子中存的数
+    Exchanger a = new Exchanger<>();
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    empty.acquire(); //等待空位
+                    mutex.acquire(); //等待读写锁
+                    apple = new Random().nextInt();
+                    System.out.println("Producing " + apple);
+                    mutex.release(); //释放读写锁
+                    full.release(); //放置产品
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    full.acquire(); //等待产品
+                    mutex.acquire(); //等待读写锁
+                    System.out.println("Consuming " + apple);
+                    mutex.release(); //释放读写锁
+                    empty.release(); //释放空位
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
+
