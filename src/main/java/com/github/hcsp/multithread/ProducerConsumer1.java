eb17cc73322e5cfc0babf3ad1bb785095eb5fe6b
yaohengfeng <1065435454@qq.com>
2020-02-19 15:24:55 +0800

    äº五种方法实现

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Object lock = new Object();
+
+        Producer producer = new Producer(lock, container);
+        Consumer consumer = new Consumer(lock, container);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,61 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        private final Object lock;
+        private Container container;
+
+        Producer(Object lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (container.getVaule().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int random = new Random().nextInt();
+                    System.out.println("Producing " + random);
+                    container.setVaule(Optional.of(random));
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private final Object lock;
+        private Container container;
+
+        Consumer(Object lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!container.getVaule().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Integer value = container.getVaule().get();
+                    container.setVaule(Optional.empty());
+                    System.out.println("Consuming " + value);
+                    lock.notify();
+                }
+            }
+        }
     }
 }
