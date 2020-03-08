e9236b24e9ca46822bd4e4c87b3048440be27622
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-03-08 15:31:28 +0800

    生产者消费者模型 (#112)
    生产者消费者模型 (#112)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,48 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    private static Exchanger<Integer> exchanger = new Exchanger<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int randomNum = new Random().nextInt();
+                    System.out.println("Producing " + randomNum);
+                    exchanger.exchange(randomNum);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Integer randomNum = exchanger.exchange(0);
+                    System.out.println("Consuming " + randomNum);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
