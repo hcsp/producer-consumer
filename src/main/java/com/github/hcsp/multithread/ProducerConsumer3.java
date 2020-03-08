d71018658202145ab51b8ee3031ac642cd48987e
Lee <lizhijie_lee@126.com>
2020-03-08 15:31:00 +0800

    多线程通信

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,73 +1,71 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class ProducerConsumer3 {
+    private static Semaphore fillQueue = new Semaphore(1);
+    private static Semaphore emptyQueue = new Semaphore(0);
+
     public static void main(String[] args) throws InterruptedException {
-        Semaphore sem = new Semaphore(1);
-        Producer producer = new Producer(sem);
-        Consumer consumer = new Consumer(sem);
+        Queue<Integer> queue = new LinkedList<>();
+
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
         producer.start();
         consumer.start();
 
         producer.join();
-        producer.join();
-    }
-
-    public static class Shared {
-        static int sharedNum = 0;
-
-        static void produceNum() {
-            sharedNum = new Random().nextInt();
-            System.out.println("Producing " + sharedNum);
-        }
-
-        static void consumeNum() {
-            System.out.println("Consuming " + sharedNum);
-        }
+        consumer.join();
     }
 
     public static class Producer extends Thread {
-        private Semaphore sem;
+        private Queue<Integer> queue;
 
-        Producer(Semaphore sem) {
-            this.sem = sem;
+        Producer(Queue<Integer> queue) {
+            this.queue = queue;
         }
 
         @Override
         public void run() {
             try {
                 for (int i = 0; i < 10; i++) {
-                    sem.acquire();
-                    Shared.produceNum();
+                    fillQueue.acquire();
+                    synchronized (ProducerConsumer3.class) {
+                        int sharedNum = new Random().nextInt();
+                        queue.add(sharedNum);
+                        System.out.println("Producing " + sharedNum);
+                    }
+                    emptyQueue.release();
                 }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            } finally {
-                sem.release();
             }
         }
     }
 
     public static class Consumer extends Thread {
-        private Semaphore sem;
+        private Queue<Integer> queue;
 
-        Consumer(Semaphore sem) {
-            this.sem = sem;
+        Consumer(Queue<Integer> queue) {
+            this.queue = queue;
         }
 
         @Override
         public void run() {
             try {
                 for (int i = 0; i < 10; i++) {
-                    sem.acquire();
-                    Shared.consumeNum();
+                    emptyQueue.acquire();
+                    synchronized (ProducerConsumer3.class) {
+                        Integer sharedNum = queue.remove();
+                        System.out.println("Consuming " + sharedNum);
+                    }
+                    fillQueue.release();
                 }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            } finally {
-                sem.release();
             }
         }
     }
