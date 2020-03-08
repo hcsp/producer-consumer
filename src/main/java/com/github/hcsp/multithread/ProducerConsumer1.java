4d968148205d0980af972655d55404f28f905b71
Lee <lizhijie_lee@126.com>
2020-03-08 14:01:40 +0800

    生产者消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static int randomNum;
+    private static boolean isProduced = false;
+
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        final Object obj = new Object();
+        Producer producer = new Producer(obj);
+        Consumer consumer = new Consumer(obj);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,63 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        private final Object obj;
+
+        Producer(Object obj) {
+            this.obj = obj;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    produceRandomNum();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void produceRandomNum() throws InterruptedException {
+            synchronized (obj) {
+                if (isProduced) {
+                    obj.wait();
+                }
+                randomNum = new Random().nextInt();
+                System.out.println("Producing " + randomNum);
+                isProduced = true;
+                obj.notifyAll();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private final Object obj;
+
+        Consumer(Object obj) {
+            this.obj = obj;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    consumeRandomNum();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void consumeRandomNum() throws InterruptedException {
+            synchronized (obj) {
+                if (!isProduced) {
+                    obj.wait();
+                }
+                isProduced = false;
+                System.out.println("Consuming " + randomNum);
+                obj.notifyAll();
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,74 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore sem = new Semaphore(1);
+        Producer producer = new Producer(sem);
+        Consumer consumer = new Consumer(sem);
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
+
+    public static class Shared {
+        static int sharedNum = 0;
+
+        static void produceNum() {
+            sharedNum = new Random().nextInt();
+            System.out.println("Producing " + sharedNum);
+        }
+
+        static void consumeNum() {
+            System.out.println("Consuming " + sharedNum);
+        }
+    }
 
     public static class Producer extends Thread {
+        private Semaphore sem;
+
+        Producer(Semaphore sem) {
+            this.sem = sem;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    sem.acquire();
+                    Shared.produceNum();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                sem.release();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Semaphore sem;
+
+        Consumer(Semaphore sem) {
+            this.sem = sem;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    sem.acquire();
+                    Shared.consumeNum();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                sem.release();
+            }
+        }
     }
 }
