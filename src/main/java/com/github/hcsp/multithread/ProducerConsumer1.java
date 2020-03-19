b0a938cb849146c9c39129257d82e0c86e229529
zhengquan <zheng_quan45@163.com>
2020-03-19 13:06:52 +0800

    5种消费者生产者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,24 +1,85 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
+/**
+ * Object.wait/notify
+ */
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container(null);
+
+        Producer producer = new Producer(container);
+        Consumer consumer = new Consumer(container);
+
 
         producer.start();
         consumer.start();
 
         producer.join();
-        producer.join();
+        consumer.join();
     }
 
+    static class Container {
+        Object value;
+
+        Container(Object value) {
+            this.value = value;
+        }
+    }
+
+
     public static class Producer extends Thread {
+        final Container container;
+
+        Producer(Container container) {
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (container) {
+                    while (container.value != null) {
+                        try {
+                            container.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Object value = container.value = new Random().nextInt();
+                    System.out.println("Producing " + value);
+                    container.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        final Container container;
+
+        Consumer(Container container) {
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (container) {
+                    while (container.value == null) {
+                        try {
+                            container.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Object value = container.value;
+                    container.value = null;
+                    System.out.println("Consuming " + value);
+                    container.notify();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,66 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Semaphore;
+
+/**
+ *  Exchanger
+ */
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Exchanger<Object> exchanger = new Exchanger();
+
+        Producer producer = new Producer(exchanger);
+        Consumer consumer = new Consumer(exchanger);
+
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+    }
 
     public static class Producer extends Thread {
+        final Exchanger<Object> exchanger;
+
+        Producer(Exchanger exchanger) {
+            this.exchanger = exchanger;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Object value = new Random().nextInt();
+                    exchanger.exchange(value);
+                    System.out.println("Producing " + value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        final Exchanger exchanger;
+
+        Consumer(Exchanger exchanger) {
+            this.exchanger = exchanger;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Object value = exchanger.exchange(null);
+                    System.out.println("Consuming " + value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
