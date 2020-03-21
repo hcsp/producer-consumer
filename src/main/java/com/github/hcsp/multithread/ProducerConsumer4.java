834a644d4bf1648833c9dffcb9ce8674cca43151
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-03-21 17:53:07 +0800

    五种方法 (#118)
    五种方法 (#118)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,93 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore producerLock = new Semaphore(1);
+        Semaphore consumerLock = new Semaphore(0);
+        Plate plate = new Plate();
+
+        Producer producer = new Producer(producerLock, consumerLock, plate);
+        Consumer consumer = new Consumer(producerLock, consumerLock, plate);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
+
+    public static class Plate {
+        private Optional value = Optional.empty();
+
+        public void setValue(Optional value) {
+            this.value = value;
+        }
+
+        public Optional getValue() {
+            return value;
+        }
+    }
+
 
     public static class Producer extends Thread {
+        private Semaphore producerLock;
+        private Semaphore consumerLock;
+        private Plate plate;
+
+        public Producer(Semaphore producerLock, Semaphore consumerLock, Plate plate) {
+            this.producerLock = producerLock;
+            this.consumerLock = consumerLock;
+            this.plate = plate;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    producerLock.acquire();
+                    Optional randomNumber = Optional.of(new Random().nextInt());
+                    plate.setValue(randomNumber);
+                    System.out.println("Producing " + randomNumber.get());
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    consumerLock.release();
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        private Semaphore producerLock;
+        private Semaphore consumerLock;
+        private Plate plate;
+
+        public Consumer(Semaphore producerLock, Semaphore consumerLock, Plate plate) {
+            this.producerLock = producerLock;
+            this.consumerLock = consumerLock;
+            this.plate = plate;
+        }
+
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    consumerLock.acquire();
+                    System.out.println("Consuming " + plate.getValue().get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    producerLock.release();
+                }
+            }
+
+        }
     }
 }
