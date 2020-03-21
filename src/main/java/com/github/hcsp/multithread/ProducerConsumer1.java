14d2c2f30acfe6a259f51071eb11c86008bc999e
wenzhe <mysteryven@gmail.com>
2020-03-21 15:43:13 +0800

    lock.wait/lock.notify 方法

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Object lock = new Object();
+        Plate plate = new Plate();
+
+
+        Producer producer = new Producer(lock, plate);
+        Consumer consumer = new Consumer(lock, plate);
 
         producer.start();
         consumer.start();
@@ -12,13 +19,74 @@ public class ProducerConsumer1 {
         producer.join();
     }
 
+    public static class Plate {
+        private Optional value = Optional.empty();
+
+        public void setValue(Optional value) {
+            this.value = value;
+        }
+
+        public Optional getValue() {
+            return value;
+        }
+    }
+
     public static class Producer extends Thread {
+        private Object lock;
+        private Plate plate;
+
+        public Producer(Object lock, Plate plate) {
+            this.lock = lock;
+            this.plate = plate;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (plate.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Optional randomNumber = Optional.of(new Random().nextInt());
+                    System.out.println("Producing " + randomNumber.get());
+                    plate.setValue(randomNumber);
+                    lock.notify();
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        private Object lock;
+        private Plate plate;
+
+        public Consumer(Object lock, Plate plate) {
+            this.lock = lock;
+            this.plate = plate;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!plate.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println("Consuming " + plate.getValue().get());
+                    plate.setValue(Optional.empty());
+                    lock.notify();
+                }
+            }
+        }
     }
 }
