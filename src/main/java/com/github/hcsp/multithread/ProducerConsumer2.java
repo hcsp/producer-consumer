b8266908a8035f2af6718bb9f6b321f0863b20f9
ARKbreeze <61083620+ARKbreeze@users.noreply.github.com>
2020-03-23 21:14:50 +0800

    完成多生产者消费者练习 (#121)
    完成多生产者消费者练习 (#121)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,121 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Container container = new Container(lock);
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+    }
+
+    public static class Container {
+        private Optional<Integer> container = Optional.empty();
+        Condition notConsumedYet;
+        Condition notProducedYet;
+
+        public Container(ReentrantLock lock) {
+            this.notConsumedYet = lock.newCondition();
+            this.notProducedYet = lock.newCondition();
+        }
+
+        public Condition getNotConsumedYet() {
+            return notConsumedYet;
+        }
+
+        public void setNotConsumedYet(Condition notConsumedYet) {
+            this.notConsumedYet = notConsumedYet;
+        }
+
+        public Condition getNotProducedYet() {
+            return notProducedYet;
+        }
+
+        public void setNotProducedYet(Condition notProducedYet) {
+            this.notProducedYet = notProducedYet;
+        }
+
+
+        public Optional<Integer> getContainer() {
+            return container;
+        }
+
+        public void setContainer(Optional<Integer> container) {
+            this.container = container;
+        }
+    }
 
     public static class Producer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Producer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (container.getContainer().isPresent()) {
+                        try {
+                            container.notConsumedYet.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Integer r = new Random().nextInt();
+                    container.setContainer(Optional.of(r));
+                    System.out.println("Producing " + r);
+                    container.notProducedYet.signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Consumer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (!container.getContainer().isPresent()) {
+                        try {
+                            container.notProducedYet.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + container.getContainer().get());
+                    container.setContainer(Optional.empty());
+                    container.notConsumedYet.signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
