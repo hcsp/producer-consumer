b0ecfcf3460c5018b3a5c4b7f071563a945a0170
linkai.huang <>
2020-03-28 16:01:06 +0800

    5种方法实现一个生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,17 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        ReentrantLock lock = new ReentrantLock();
+        Container container = new Container(lock);
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
@@ -13,12 +21,107 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        private Container container;
+        private ReentrantLock lock;
+
+        public Producer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (container.getValue().isPresent()) {
+                        try {
+                            container.getNotConsumer().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int r = new Random().nextInt();
+                    System.out.println("Producing " + r);
+                    container.setValue(Optional.of(r));
+                    container.getNotProduce().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Container container;
+        private ReentrantLock lock;
+
+        public Consumer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            container.getNotProduce().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Integer value = container.getValue().get();
+                    System.out.println("Consuming " + value);
+                    container.setValue(Optional.empty());
+
+                    container.getNotConsumer().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    static class Container {
+        private Condition notConsumer;
+        private Condition notProduce;
+
+        Container(ReentrantLock lock) {
+            this.notConsumer = lock.newCondition();
+            this.notProduce = lock.newCondition();
+        }
+
+        public Condition getNotConsumer() {
+            return notConsumer;
+        }
+
+        public void setNotConsumer(Condition notConsumer) {
+            this.notConsumer = notConsumer;
+        }
+
+        public Condition getNotProduce() {
+            return notProduce;
+        }
+
+        public void setNotProduce(Condition notProduce) {
+            this.notProduce = notProduce;
+        }
+
+
+        Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
