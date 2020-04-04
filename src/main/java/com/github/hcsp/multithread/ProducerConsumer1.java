380f423c2419fee05e19ca3b358179d88ba672b3
samuel <zhangleven@gmail.com>
2020-04-05 03:39:26 +0800

    practise producer-consumer

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Object lock = new Object();
+        Box<Integer> box = new Box<>();
+
+        Producer producer = new Producer(lock, box);
+        Consumer consumer = new Consumer(lock, box);
 
         producer.start();
         consumer.start();
@@ -13,12 +18,60 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        private Object lock;
+        private Box<Integer> box;
+
+        public Producer(Object lock, Box<Integer> box) {
+            this.lock = lock;
+            this.box = box;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!box.isEmpty()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    int value = new Random().nextInt();
+                    System.out.println("Producing " + value);
+                    box.put(value);
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Object lock;
+        private Box<Integer> box;
+
+        public Consumer(Object lock, Box<Integer> box) {
+            this.lock = lock;
+            this.box = box;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (box.isEmpty()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    System.out.println("Consuming " + box.take());
+                    lock.notify();
+                }
+            }
+        }
     }
 }
