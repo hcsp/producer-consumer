d5b196ab9ad2644ab8d793920824400c29b95049
gofocus <go_focus@live.cn>
2020-04-10 09:52:48 +0800

    多线程练习4：实现一个生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static final Object LOCK = new Object();
+    private static Optional<Integer> container = Optional.empty();
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -14,11 +20,43 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (LOCK) {
+                    while (container.isPresent()) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int anInt = new Random().nextInt();
+                    System.out.println("Producing " + anInt);
+                    container = Optional.of(anInt);
+                    LOCK.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (LOCK) {
+                    while (!container.isPresent()) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println("Consuming " + container.get());
+                    container = Optional.empty();
+                    LOCK.notify();
+                }
+            }
+        }
     }
 }
