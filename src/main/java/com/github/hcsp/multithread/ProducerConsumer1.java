f169b0f9f49257361d18ffb1dd7069e86013ae78
dandanloveJM <gigi834921748@qq.com>
2020-04-13 01:00:10 +0100

    5种方法实现生产者消费者

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,17 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Basket basket = new Basket();
+        Object lock = new Object();
+
+        Producer.activeCount();
+        Producer producer = new Producer(basket, lock);
+        Consumer consumer = new Consumer(basket, lock);
+
 
         producer.start();
         consumer.start();
@@ -13,12 +21,80 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Basket basket;
+        Object lock;
+
+        public Producer(Basket basket, Object lock) {
+            this.basket = basket;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (basket.getBasket().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int r = new Random().nextInt();
+                    System.out.println("Producing " + r);
+                    basket.setBasket(Optional.of(r));
+
+                    lock.notify();
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        Basket basket;
+        Object lock;
+
+        public Consumer(Basket basket, Object lock) {
+            this.basket = basket;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!basket.getBasket().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Integer value = this.basket.getBasket().get();
+                    basket.setBasket(Optional.empty());
+                    System.out.println("consuming " + value);
+
+                    lock.notify();
+
+
+                }
+            }
+
+        }
+    }
+
+    public static class Basket {
+        Optional<Integer> basket = Optional.empty();
+
+        public Optional<Integer> getBasket() {
+            return basket;
+        }
+
+        public void setBasket(Optional<Integer> basket) {
+            this.basket = basket;
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,84 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Exchanger<Integer> exchanger = new Exchanger<>();
+        Container container = new Container();
+        Producer producer = new Producer(exchanger, container);
+        Consumer consumer = new Consumer(exchanger, container);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        Exchanger<Integer> exchanger;
+        Container container;
+
+        public Producer(Exchanger<Integer> exchanger, Container container) {
+            this.exchanger = exchanger;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+
+            for (int i = 0; i < 10; i++) {
+                int r = new Random().nextInt();
+                container.setValue(Optional.of(r));
+                try {
+                    Thread.sleep(1000);
+                    exchanger.exchange(r);
+                    System.out.println("Producing " + r);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Exchanger<Integer> exchanger;
+        Container container;
+
+        public Consumer(Exchanger<Integer> exchanger, Container container) {
+            this.exchanger = exchanger;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(1000);
+                    Integer value = exchanger.exchange(null);
+                    System.out.println("Consuming " + value);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
+
+    public static class Container {
+        Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
+    }
+
+
 }
