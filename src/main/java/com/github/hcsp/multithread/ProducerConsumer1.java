7d7ed48d9251141436ba2c7dd357c03495109996
hah <1019746812@qq.com>
2020-04-21 00:11:54 +0800

    五种方法实现生产者和消费者

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
+/**
+ *  Object.wait/notify
+ */
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Producer producer = new Producer(container);
+        Consumer consumer = new Consumer(container);
 
         producer.start();
         consumer.start();
@@ -12,13 +18,62 @@ public class ProducerConsumer1 {
         producer.join();
     }
 
+    static class Container {
+        Object value;
+    }
+
     public static class Producer extends Thread {
+        Container container;
+
+        Producer(Container container) {
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (container) {
+                    while (container.value != null) {
+                        try {
+                            container.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int val = new Random().nextInt();
+                    container.value = val;
+                    System.out.println("Producing " + val);
+                    container.notify();
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+
+        Consumer(Container container) {
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (container) {
+                    while (container.value == null) {
+                        try {
+                            container.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    System.out.println("Consuming " + container.value);
+                    container.value = null;
+                    container.notify();
+                }
+            }
+
+        }
     }
 }
