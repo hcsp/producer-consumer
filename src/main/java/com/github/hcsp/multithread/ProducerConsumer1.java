02735cfbcf8eac00fe1540e6b11b697e4ea14df3
Sunpeng526 <215860865@qq.com>
2020-04-24 01:19:49 -0700

    submit produce and consume code

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,24 +1,78 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
+/* Object.wait/notify */
 public class ProducerConsumer1 {
+
+    static Object lock = new Object();
+
+    public static class ValueObject {
+        public static int value = 0;
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+
+        Producer producer = new Producer(lock);
+        Consumer consumer = new Consumer(lock);
 
         producer.start();
         consumer.start();
-
-        producer.join();
-        producer.join();
     }
 
     public static class Producer extends Thread {
+
+        Object lock;
+
+        Producer(Object lock) {
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    while (ValueObject.value != 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                    ValueObject.value = new Random().nextInt();
+                    System.out.println("Producing " + ValueObject.value);
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+
+        Object lock;
+
+        Consumer(Object lock) {
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    while (ValueObject.value == 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + ValueObject.value);
+                    ValueObject.value = 0;
+                    lock.notify();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,65 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
+/* Exchanger */
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+
+    public static class ValueObject {
+        public static int value = 0;
+    }
+
+    static Exchanger<Integer> exchanger = new Exchanger<Integer>();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Producer producer = new Producer(exchanger);
+        Consumer consumer = new Consumer(exchanger);
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(producer).start();
+            new Thread(consumer).start();
+        }
+    }
 
     public static class Producer extends Thread {
+
+        Exchanger<Integer> exchanger;
+
+        public Producer(Exchanger<Integer> exchanger) {
+            this.exchanger = exchanger;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                ValueObject.value = new Random().nextInt();
+                System.out.println("Producing " + ValueObject.value);
+                exchanger.exchange(ValueObject.value);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+
+        Exchanger<Integer> exchanger;
+
+        public Consumer(Exchanger<Integer> exchanger) {
+            this.exchanger = exchanger;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                System.out.println("Consuming " + exchanger.exchange(ValueObject.value));
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
