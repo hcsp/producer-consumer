5e8c7d1e04c7f5bfedee511390b192dd5e761978
zhangzh <zhangzh@cnlemon.net>
2020-05-16 10:09:03 +0800

    feat: 完成挑战

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(1);
+        final Integer TARGET_NUM = 10;
+        Producer producer = new Producer(arrayBlockingQueue, TARGET_NUM);
+        Consumer consumer = new Consumer(arrayBlockingQueue, TARGET_NUM);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,52 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        BlockingQueue<Integer> blockingQueue;
+        final Integer TARGET_NUM;
+
+        public Producer(BlockingQueue<Integer> blockingQueue, Integer TARGET_NUM) {
+            this.blockingQueue = blockingQueue;
+            this.TARGET_NUM = TARGET_NUM;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < TARGET_NUM; i++) {
+                try {
+                    Integer randomProduct = getRandomProduct();
+                    blockingQueue.put(randomProduct);
+                    System.out.println("Producing " + randomProduct);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Integer getRandomProduct() {
+            return new Random().nextInt();
+        }
     }
 
     public static class Consumer extends Thread {
+
+        BlockingQueue<Integer> blockingQueue;
+        final Integer TARGET_NUM;
+
+        public Consumer(BlockingQueue<Integer> blockingQueue, Integer TARGET_NUM) {
+            this.blockingQueue = blockingQueue;
+            this.TARGET_NUM = TARGET_NUM;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < TARGET_NUM; i++) {
+                try {
+                    Integer product = blockingQueue.take();
+                    System.out.println("Consuming " + product);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
