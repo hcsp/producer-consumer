2e01f091cc44d6aa0ce54a64aa45e74d34693845
kongmu373 <kongmu1235@gmail.com>
2020-06-02 16:17:33 +0800

    实现生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static final Object LOCK = new Object();
+
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Optional<Integer> value = Optional.empty();
+        Container container = new Container(value);
+        Producer producer = new Producer(LOCK, container);
+        Consumer consumer = new Consumer(LOCK, container);
 
         producer.start();
         consumer.start();
@@ -13,12 +20,67 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        final Object lock;
+        Container container;
+
+        public Producer(Object lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (container.value.isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int r = new Random().nextInt();
+                    container.value = Optional.of(r);
+                    System.out.println("Producing " + r);
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        final Object lock;
+        Container container;
+
+        public Consumer(Object lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!container.value.isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    System.out.println("Consuming " + container.value.get());
+                    container.value = Optional.empty();
+                    lock.notify();
+                }
+            }
+        }
+    }
+
+    public static class Container {
+        Optional<Integer> value;
+
+        public Container(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,94 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Optional<Integer> value = Optional.empty();
+        Container container = new Container(value, lock);
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Producer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (container.value.isPresent()) {
+                        container.notFull.await();
+                    }
+                    int r = new Random().nextInt();
+                    System.out.println("Producing " + r);
+                    container.value = Optional.of(r);
+                    container.notEmpty.signal();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Consumer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (!container.value.isPresent()) {
+                        container.notEmpty.await();
+                    }
+                    System.out.println("Consuming " + container.value.get());
+                    container.value = Optional.empty();
+                    container.notFull.signal();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    lock.unlock();
+                }
+            }
+
+        }
+    }
+
+    public static class Container {
+        Optional<Integer> value;
+        Condition notFull;
+        Condition notEmpty;
+
+        public Container(Optional<Integer> value, ReentrantLock lock) {
+            this.value = value;
+            this.notFull = lock.newCondition();
+            this.notEmpty = lock.newCondition();
+        }
     }
 }
