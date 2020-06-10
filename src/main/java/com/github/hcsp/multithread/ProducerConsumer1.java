d2f29e5b549e283f8f9f1bfd4a5fb2a8b8d7e119
郑海川 <53638659+lance-zheng66@users.noreply.github.com>
2020-06-10 11:26:34 +0800

    生产者与消费者模型的三种实现模式 (#148)
    生产者与消费者模型的三种实现模式 (#148)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,24 +1,103 @@
 package com.github.hcsp.multithread;
 
+
+import com.github.hcsp.multithread.ProducerConsumer1.Producer.Consumer;
+import com.github.hcsp.multithread.ProducerConsumer1.Producer.Container;
+import java.util.Random;
+
 public class ProducerConsumer1 {
-    public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
 
-        producer.start();
-        consumer.start();
+  private static final Object LOCK = new Object();
 
-        producer.join();
-        producer.join();
+  public static void main(String[] args) throws InterruptedException {
+
+    Container container = new Container();
+
+    Producer producer = new Producer(container, LOCK);
+    Consumer consumer = new Consumer(container, LOCK);
+
+    producer.start();
+    consumer.start();
+
+    producer.join();
+    producer.join();
+  }
+
+  public static class Producer extends Thread {
+
+    private final Container container;
+    private final Object LOCK;
+
+    public Producer(Container container, Object LOCK) {
+      this.container = container;
+      this.LOCK = LOCK;
     }
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+
+    @Override
+    public void run() {
+      for (int i = 0; i < 10; i++) {
+        synchronized (LOCK) {
+          if (container.getItem() != null) {
+            try {
+              LOCK.wait();
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+          int item = new Random().nextInt();
+          container.setItem(item);
+          System.out.println("Producing " + item);
+          LOCK.notify();
+        }
+      }
     }
 
     public static class Consumer extends Thread {
-        @Override
-        public void run() {}
+
+      private final Container container;
+      private final Object LOCK;
+
+      public Consumer(Container container, Object LOCK) {
+        this.container = container;
+        this.LOCK = LOCK;
+      }
+
+      @Override
+      public void run() {
+        for (int i = 0; i < 10; i++) {
+          synchronized (LOCK) {
+            if (container.getItem() == null) {
+              try {
+                LOCK.wait();
+              } catch (InterruptedException e) {
+                e.printStackTrace();
+              }
+            }
+            // 消费
+            System.out.println("Consuming " + container.getItem());
+            container.setItem(null);
+            LOCK.notify();
+          }
+        }
+      }
     }
+
+    static class Container {
+
+      private Integer item;
+
+      public Integer getItem() {
+        return item;
+      }
+
+      public void setItem(Integer item) {
+        this.item = item;
+      }
+    }
+
+  }
 }
+
+
+
