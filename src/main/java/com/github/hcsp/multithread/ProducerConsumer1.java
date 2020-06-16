9a6a24678a693261e7ae06f40c3f4ca7a0e961a7
lazyben <458861463@qq.com>
2020-06-16 22:07:05 +0800

    实现一个生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Object lock = new Object();
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
@@ -13,12 +18,72 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        private final Container container;
+        private final Object lock;
+
+        public Producer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int j = 0; j < 10; j++) {
+                synchronized (lock) {
+                    while (container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Integer i = new Random().nextInt();
+                    System.out.println("Producing " + i);
+                    container.setValue(Optional.of(i));
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private final Container container;
+        private final Object lock;
+
+        public Consumer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int j = 0; j < 10; j++) {
+                synchronized (lock) {
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + container.getValue().get());
+                    container.setValue(Optional.empty());
+                    lock.notify();
+                }
+            }
+        }
+    }
+
+    private static class Container {
+        //从 Java 8 引入的一个很有趣的特性是 Optional 类。Optional 类主要解决的问题是臭名昭著的空指针异常（NullPointerException
+        Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
