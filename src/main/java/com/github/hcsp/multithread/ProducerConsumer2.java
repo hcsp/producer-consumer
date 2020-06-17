76052cfb4927f927096dda2613ed482182ce6500
Haixiang <haixiang6123@gmail.com>
2020-06-17 14:47:04 +0800

    实现一个生产者-消费者模型 (#151)
    实现一个生产者-消费者模型 (#151)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,120 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Container container = new Container(lock);
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
+
+    public static class Container {
+        private Condition notConsumedYet;
+        private Condition notProducedYet;
+
+        private Optional<Integer> value = Optional.empty();
+
+        public Container(ReentrantLock lock) {
+            this.notConsumedYet = lock.newCondition();
+            this.notProducedYet = lock.newCondition();
+        }
+
+        public Condition getNotConsumedYet() {
+            return notConsumedYet;
+        }
+
+        public Condition getNotProducedYet() {
+            return notProducedYet;
+        }
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
+    }
 
     public static class Producer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Producer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+
+                    while (container.getValue().isPresent()) {
+                        try {
+                            container.getNotConsumedYet().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int product = new Random().nextInt();
+                    System.out.println("Producing " + product);
+                    container.setValue(Optional.of(product));
+
+                    container.getNotProducedYet().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        ReentrantLock lock;
+
+        public Consumer(Container container, ReentrantLock lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            container.getNotProducedYet().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    Integer product = container.getValue().get();
+                    container.setValue(Optional.empty());
+                    System.out.println("Consuming " + product);
+
+                    container.getNotConsumedYet().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
