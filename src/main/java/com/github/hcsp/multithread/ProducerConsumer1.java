d1eca830b0c7dec80233f375d008d177c5595206
walkerxin <44486634+walkerxin@users.noreply.github.com>
2020-08-01 00:50:25 +0800

    implement Producer consumer pattern (#161)
    implement Producer consumer pattern (#161)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static Optional<Integer> container = Optional.empty();
+    private static final Object LOCK = new Object();
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -9,16 +15,48 @@ public class ProducerConsumer1 {
         consumer.start();
 
         producer.join();
-        producer.join();
+        consumer.join();
     }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (LOCK) {
+                    while (container.orElse(null) != null) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    // Perform action appropriate to condition
+                    int num = new Random().nextInt();
+                    container = Optional.of(num);
+                    System.out.println("Producing " + num);
+                    LOCK.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (LOCK) {
+                    while (!container.isPresent()) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + container.get());
+                    container = Optional.empty();
+                    LOCK.notify();
+                }
+            }
+        }
     }
 }
