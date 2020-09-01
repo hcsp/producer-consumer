aa035f2f04f807f980d22baa442f36b2c7d108a6
lihua <lihua@123.com>
2020-09-02 02:15:59 +0800

    生产者与消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,12 +1,12 @@
 package com.github.hcsp.multithread;
 
 import java.util.Random;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ProducerConsumer3 {
     public static void main(String[] args) throws InterruptedException {
-        LinkedBlockingDeque<Integer> queue = new LinkedBlockingDeque<>(1);
-        LinkedBlockingDeque<Integer> signalQueue = new LinkedBlockingDeque<>(1);
+        LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
+        LinkedBlockingQueue<Integer> signalQueue = new LinkedBlockingQueue<>(1);
 
         Producer producer = new Producer(queue, signalQueue);
         Consumer consumer = new Consumer(queue, signalQueue);
@@ -19,19 +19,19 @@ public class ProducerConsumer3 {
     }
 
     public static class Producer extends Thread {
-        LinkedBlockingDeque<Integer> signalQueue;
-        LinkedBlockingDeque<Integer> queue;
+        LinkedBlockingQueue<Integer> queue;
+        LinkedBlockingQueue<Integer> signalQueue;
 
-        public Producer(LinkedBlockingDeque<Integer> signalQueue, LinkedBlockingDeque<Integer> queue) {
-            this.signalQueue = signalQueue;
+        public Producer(LinkedBlockingQueue<Integer> queue, LinkedBlockingQueue<Integer> signalQueue) {
             this.queue = queue;
+            this.signalQueue = signalQueue;
         }
 
         @Override
         public void run() {
             for (int i = 0; i < 10; i++) {
-                int r = new Random().nextInt();
-                System.out.println("Producing" + r);
+                Integer r = new Random().nextInt();
+                System.out.println("Producing " + r);
                 try {
                     queue.put(r);
                     signalQueue.take();
@@ -43,20 +43,21 @@ public class ProducerConsumer3 {
     }
 
     public static class Consumer extends Thread {
-        LinkedBlockingDeque<Integer> queue;
-        LinkedBlockingDeque<Integer> signalQueue;
+        LinkedBlockingQueue<Integer> queue;
+        LinkedBlockingQueue<Integer> signalQueue;
 
-        public Consumer(LinkedBlockingDeque<Integer> queue, LinkedBlockingDeque<Integer> signalQueue) {
+        public Consumer(LinkedBlockingQueue<Integer> queue, LinkedBlockingQueue<Integer> signalQueue) {
             this.queue = queue;
             this.signalQueue = signalQueue;
         }
+
 
         @Override
         public void run() {
             for (int i = 0; i < 10; i++) {
                 try {
                     Integer r = queue.take();
-                    System.out.println("Consuming" + r);
+                    System.out.println("Consuming " + r);
                     signalQueue.put(0);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
