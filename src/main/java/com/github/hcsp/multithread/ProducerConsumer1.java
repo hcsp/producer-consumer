5653f0abc03b69dbee01da26e400c0073ea61be2
spareGuiHub <70204176+spareGuiHub@users.noreply.github.com>
2020-09-08 16:53:28 +0800

    生产者-消费者模型 (#168)
    生产者-消费者模型 (#168)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        final Object LOCK = new Object();
+        Container container = new Container();
+
+        Producer producer = new Producer(LOCK, container);
+        Consumer consumer = new Consumer(LOCK, container);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,72 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        final Object LOCK;
+        Container container;
+
+        public Producer(Object LOCK, Container container) {
+            this.LOCK = LOCK;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (LOCK) {
+                    while (container.getValue().isPresent()) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int r = new Random().nextInt();
+                    System.out.println("Producing " + r);
+                    container.setValue(Optional.of(r));
+                    LOCK.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        final Object LOCK;
+        Container container;
+
+        public Consumer(Object LOCK, Container container) {
+            this.LOCK = LOCK;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (LOCK) {
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int r = container.getValue().get();
+                    System.out.println("Consuming " + r);
+                    container.setValue(Optional.empty());
+                    LOCK.notify();
+                }
+            }
+        }
+    }
+
+    public static class Container {
+        Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
