e217e19c998776c2e3344548dd3b83bdedc02648
chandler <596202898@qq.com>
2020-09-16 00:09:20 +0800

    完成5种生产者消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,13 @@
 package com.github.hcsp.multithread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+
+    static Integer num;
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -14,11 +21,57 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int count = 0;
+            while (true) {
+                synchronized (ProducerConsumer1.class) {
+                    if (count == 10) {
+                        break;
+                    }
+                    if (num == null) {
+                        num = new Random().nextInt();
+                        System.out.println("Producing " + num);
+                        count++;
+                        ProducerConsumer1.class.notify();
+                    } else {
+                        try {
+                            ProducerConsumer1.class.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int temp;
+            int count = 0;
+            while (true) {
+                synchronized (ProducerConsumer1.class) {
+                    if (count == 10) {
+                        break;
+                    }
+                    if (num == null) {
+                        try {
+                            ProducerConsumer1.class.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+
+                    } else {
+                        temp = num;
+                        num = null;
+                        count++;
+                        System.out.println("Consuming " + temp);
+                        ProducerConsumer1.class.notify();
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,57 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>(1);
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int count = 0;
+            while (true) {
+                if (count == 10) {
+                    break;
+                }
+                int num = new Random().nextInt();
+                System.out.println("Producing " + num);
+                count++;
+                try {
+                    queue.put(num);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int count = 0;
+            while (true) {
+                if (count == 10) {
+                    break;
+                }
+                try {
+                    count++;
+                    System.out.println("Consuming " + queue.take());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
