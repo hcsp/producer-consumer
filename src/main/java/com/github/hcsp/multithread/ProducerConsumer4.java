163d9010d1a98167beb36d7a612486116ce2d65d
Chandler <40543122+heyble@users.noreply.github.com>
2020-09-16 23:12:16 +0800

    完成5种生产者消费者模型 (#170)
    完成5种生产者消费者模型 (#170)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,62 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static Semaphore producer = new Semaphore(1);
+    static Semaphore consumer = new Semaphore(0);
+    static Integer num;
+       public static void main(String[] args) throws InterruptedException {
+           Producer producer = new Producer();
+           Consumer consumer = new Consumer();
+
+           producer.start();
+           consumer.start();
+
+           producer.join();
+           producer.join();
+       }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int count = 0;
+            while (true) {
+                if (count == 10) {
+                    break;
+                }
+                try {
+                    producer.acquire(1);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                num = new Random().nextInt();
+                System.out.println("Producing " + num);
+                count++;
+                consumer.release();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int count = 0;
+            while (true) {
+                if (count == 10) {
+                    break;
+                }
+                try {
+                    consumer.acquire(1);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                System.out.println("Consuming " + num);
+                num = null;
+                count++;
+                producer.release();
+            }
+        }
     }
 }
