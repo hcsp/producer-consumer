a07b5161f17f341df84af69a0e5106cd3be0f03c
Nat Chen <chen_natalie@outlook.com>
2020-09-19 23:15:55 +0800

    Producer and consumer (#171)
    Producer and consumer (#171)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,113 @@
 package com.github.hcsp.multithread;
 
+import javax.swing.*;
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Container container = new Container(lock);
+
+        Producer producer = new Producer(lock, container);
+        Consumer consumer = new Consumer(lock, container);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        private ReentrantLock lock;
+        private Container container;
+
+        public Producer(ReentrantLock lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (container.getValue().isPresent()) {
+                        try {
+                            container.getNotConsumedYet().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int randomNumber = new Random().nextInt();
+                    container.setValue(Optional.of(randomNumber));
+                    System.out.println("Producing " + randomNumber);
+                    container.getNotProducedYet().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private ReentrantLock lock;
+        private Container container;
+
+        public Consumer(ReentrantLock lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            container.getNotProducedYet().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + container.getValue().get());
+                    container.setValue(Optional.empty());
+                    container.getNotConsumedYet().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    public static class Container {
+        private Condition notConsumedYet;
+        private Condition notProducedYet;
+        private Optional<Integer> value = Optional.empty();
+
+        public Container(ReentrantLock lock) {
+            this.notConsumedYet = lock.newCondition();
+            this.notProducedYet = lock.newCondition();
+        }
+
+        public Condition getNotConsumedYet() {
+            return notConsumedYet;
+        }
+
+        public Condition getNotProducedYet() {
+            return notProducedYet;
+        }
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
