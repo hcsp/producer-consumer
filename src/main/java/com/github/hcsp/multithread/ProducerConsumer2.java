c127797ce7b13af6f9638ac1de4dd1b7e9bd716e
gongxuanzhang <48286053+514190950@users.noreply.github.com>
2020-10-13 14:27:07 +0800

    用十种方法实现生产者消费者模型 (#174)
    用十种方法实现生产者消费者模型 (#174)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,76 @@
 package com.github.hcsp.multithread;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/***
+ *  Lock的Condition通知
+ *  @author gongxuanzhang
+ **/
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+
+    static final Queue<Integer> QUEUE = new LinkedList<>();
+
+    static Lock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (!QUEUE.isEmpty()) {
+                        condition.await();
+                    }
+                    int random = new Random().nextInt();
+                    QUEUE.offer(random);
+                    System.out.println("Producing " + random);
+                    condition.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (QUEUE.isEmpty()) {
+                        condition.await();
+                    }
+                    System.out.println("Consuming " + QUEUE.poll());
+                    condition.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
+
 }
---
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer7.java
@@ -0,0 +1,77 @@
+package com.github.hcsp.multithread;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/***
+ *  使用栅栏实现
+ *  需要用到自旋 不依赖其他类和方法的情况下还没想到好的解决办法
+ *  @author gongxuanzhang
+ **/
+public class ProducerConsumer7 {
+
+    static Queue<Integer> queue = new LinkedList<>();
+
+    static CyclicBarrier[] cyclicBarriers = new CyclicBarrier[11];
+
+    static {
+        for (int i = 0; i < 11; i++) {
+            cyclicBarriers[i] = new CyclicBarrier(2);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+
+    }
+
+    public static class Producer extends Thread {
+
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    while (!queue.isEmpty()) {
+                        Thread.sleep(1);
+                    }
+                    int random = new Random().nextInt();
+                    queue.offer(random);
+                    System.out.println("Producing " + random);
+                    cyclicBarriers[0].await();
+                }
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class Consumer extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    while (queue.isEmpty()) {
+                        Thread.sleep(1);
+                    }
+                    System.out.println("Consuming " + queue.poll());
+                    cyclicBarriers[0].await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
