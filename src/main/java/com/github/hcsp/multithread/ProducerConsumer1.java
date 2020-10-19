1fc52ec1351b6034b6f96496f392c9559d9c92b8
biantaiwen <biantaiwen@163.com>
2020-10-19 18:17:32 +0800

    done1 (#175)
    done1 (#175)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(1);
+
+        Producer producer = new Producer(blockingQueue);
+        Consumer consumer = new Consumer(blockingQueue);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,44 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        private final BlockingQueue queue;
+
+        Producer(BlockingQueue queue) {
+            this.queue = queue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+
+            try {
+                for (int i = 0; i < 100; i++) {
+                    int anInt = new Random().nextInt();
+                    queue.put(anInt);
+                    System.out.println("Producing " + anInt);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private final BlockingQueue queue;
+
+        Consumer(BlockingQueue queue) {
+            this.queue = queue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                for (int i = 0; i < 100; i++) {
+                    int take = (Integer) queue.take();
+                    System.out.println("Consuming " + take);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
