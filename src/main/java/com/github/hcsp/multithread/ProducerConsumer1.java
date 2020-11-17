38ef237606358227b916dba18b3de1d461f35d08
catmmao <catmmao@gmail.com>
2020-11-17 11:37:17 +0800

    feat: Producer-Consumer solution using wait and notify

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,13 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    static final Object lock = new Object();
+    static LinkedList<Integer> list = new LinkedList<>();
+    static final int CAPACITY = 10;
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -9,16 +16,55 @@ public class ProducerConsumer1 {
         consumer.start();
 
         producer.join();
-        producer.join();
+        consumer.join();
     }
 
-    public static class Producer extends Thread {
+    static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                int i = 0;
+                while (i++ < CAPACITY) {
+                    synchronized (lock) {
+                        if (!list.isEmpty()) {
+                            lock.wait();
+                        }
+
+                        int value = new Random().nextInt();
+                        list.add(value);
+                        System.out.println("Producing " + value);
+
+                        lock.notify();
+                        Thread.sleep(1000);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static class Consumer extends Thread {
+    static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                int i = 0;
+                while (i++ < CAPACITY) {
+                    synchronized (lock) {
+                        if (list.isEmpty()) {
+                            lock.wait();
+                        }
+
+                        int value = list.removeFirst();
+                        System.out.println("Consuming " + value);
+
+                        lock.notify();
+                        Thread.sleep(1000);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
