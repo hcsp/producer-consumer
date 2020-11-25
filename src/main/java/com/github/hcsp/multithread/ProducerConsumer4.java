8c3a4ddd9e41269ecacdef1376b7c6f908e61eaf
hank <619805205@qq.com>
2020-11-25 16:34:28 +0800

    生产者消费者5种方式

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,79 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+  public static void main(String[] args) throws InterruptedException {
+    LinkedList<Integer> list = new LinkedList<>();
+    Semaphore notFull = new Semaphore(1);
+    Semaphore notEmpty = new Semaphore(0);
+    Producer producer = new Producer(list, notFull, notEmpty);
+    Consumer consumer = new Consumer(list, notFull, notEmpty);
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+    producer.start();
+    consumer.start();
+
+    producer.join();
+    producer.join();
+  }
+
+  public static class Producer extends Thread {
+    LinkedList<Integer> list;
+    Semaphore notFull;
+    Semaphore notEmpty;
+
+    public Producer(LinkedList<Integer> list, Semaphore notFull, Semaphore notEmpty) {
+      this.list = list;
+      this.notFull = notFull;
+      this.notEmpty = notEmpty;
     }
 
-    public static class Consumer extends Thread {
-        @Override
-        public void run() {}
+    @Override
+    public void run() {
+      for (int i = 0; i < 10; i++) {
+        try {
+          notFull.acquire();
+          int r = new Random().nextInt();
+          System.out.println("producing " + r);
+          list.add(r);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } finally {
+          notEmpty.release();
+        }
+      }
+
     }
+  }
+
+  public static class Consumer extends Thread {
+
+    LinkedList<Integer> list;
+    Semaphore notFull;
+    Semaphore notEmpty;
+
+    public Consumer(LinkedList<Integer> list, Semaphore notFull, Semaphore notEmpty) {
+      this.list = list;
+      this.notFull = notFull;
+      this.notEmpty = notEmpty;
+    }
+
+    @Override
+    public void run() {
+      for (int i = 0; i < 10; i++) {
+        try {
+          notEmpty.acquire();
+          int value = list.get(0);
+          System.out.println("Consuming " + value);
+          list.remove(0);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        } finally {
+          notFull.release();
+        }
+      }
+    }
+  }
 }
