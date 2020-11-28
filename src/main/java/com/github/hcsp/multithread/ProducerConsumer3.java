2592aa4141f51afa7102d8a3849572454e2b1c16
sgz886 <sgz886@gmail.com>
2020-11-28 21:15:11 +0800

    3种方法实现生产者-消费者模型 (#178)
    3种方法实现生产者-消费者模型 (#178)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,46 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
-
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+    public static void main(String[] args) throws InterruptedException {
+        BlockingDeque<Integer> integerQueue = new LinkedBlockingDeque<>(1);
+        // BlockingDeque<Integer> signal = new LinkedBlockingDeque<>(1);
+        Thread producing = new Thread(() -> threadProduce(integerQueue), "Producing");
+        producing.start();
+        Thread consuming = new Thread(() -> threadConsume(integerQueue), "Consuming");
+        consuming.start();
+        producing.join();
+        consuming.join();
     }
 
-    public static class Consumer extends Thread {
-        @Override
-        public void run() {}
+    static void threadProduce(BlockingDeque<Integer> integerQueue) {
+        for (int i = 0; i < 10; i++) {
+            int temp = new Random().nextInt(100);
+            try {
+                integerQueue.put(temp);
+                System.out.printf("%s %d%n",
+                        Thread.currentThread().getName(),
+                        temp);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    static void threadConsume(BlockingDeque<Integer> integerQueue) {
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.printf("%s %d%n",
+                        Thread.currentThread().getName(),
+                        integerQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
