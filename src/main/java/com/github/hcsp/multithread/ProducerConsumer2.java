25471932c899016267b2568729c767ba658acb43
pccmast <35651458+pccmast@users.noreply.github.com>
2020-12-10 14:21:50 +0800

    user five ways to implement (#180)
    user five ways to implement (#180)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,92 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Condition notFull = lock.newCondition();
+        Condition notEmpty = lock.newCondition();
+        Container container = new Container();
+
+        Producer producer = new Producer(container, lock, notFull, notEmpty);
+        Consumer consumer = new Consumer(container, lock, notFull, notEmpty);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        Container container;
+        ReentrantLock lock;
+        Condition notFull;
+        Condition notEmpty;
+
+        public Producer(Container container, ReentrantLock lock, Condition notFull, Condition notEmpty) {
+            this.container = container;
+            this.lock = lock;
+            this.notFull = notFull;
+            this.notEmpty = notEmpty;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (container.getValue().isPresent()) {
+                        notFull.await();
+                    }
+                    int tempInt = new Random().nextInt();
+                    container.setValue(Optional.of(tempInt));
+                    System.out.println("Producing " + tempInt);
+                    notEmpty.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        ReentrantLock lock;
+        Condition notFull;
+        Condition notEmpty;
+
+        public Consumer(Container container, ReentrantLock lock, Condition notFull, Condition notEmpty) {
+            this.container = container;
+            this.lock = lock;
+            this.notFull = notFull;
+            this.notEmpty = notEmpty;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    lock.lock();
+                    while (!container.getValue().isPresent()) {
+                        notEmpty.await();
+                    }
+                    int tempInt = container.getValue().get();
+                    System.out.println("Consuming " + tempInt);
+                    container.setValue(Optional.empty());
+                    notFull.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,60 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+public class ProducerConsumer3 {
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
     }
 
     public static class Consumer extends Thread {
+        BlockingQueue<Integer> queue;
+
+        public Consumer(BlockingQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Producing " + queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class Producer extends Thread {
+        BlockingQueue<Integer> queue;
+
+        public Producer(BlockingQueue<Integer> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int tempInt;
+                try {
+                    tempInt = new Random().nextInt();
+                    queue.put(tempInt);
+                    System.out.println("Producing " + tempInt);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
