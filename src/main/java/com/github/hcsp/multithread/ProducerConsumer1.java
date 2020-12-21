f8b1aaf36895cf0c00f9f52c1e5c68ef1797003f
yang song <1479867360@qq.com>
2020-12-21 16:51:06 +0800

    实现 生产者-消费者模型 (#182)
    实现 生产者-消费者模型 (#182)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,10 +1,16 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer1 {
-    public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class ProducerConsumer1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Object lock = new Object();
+        AtomicInteger sign = new AtomicInteger(0);
+        AtomicInteger val = new AtomicInteger(0);
+        Producer producer = new Producer(lock, sign, val);
+        Consumer consumer = new Consumer(lock, sign, val);
         producer.start();
         consumer.start();
 
@@ -13,12 +19,65 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        final Object lock;
+        AtomicInteger sign;
+        AtomicInteger val;
+
+        public Producer(Object lock, AtomicInteger sign, AtomicInteger val) {
+            this.lock = lock;
+            this.sign = sign;
+            this.val = val;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    while (sign.get() != 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    val.set(new Random().nextInt());
+                    System.out.println("Producing " + val);
+                    sign.set(1);
+                    lock.notify();
+                }
+
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        final Object lock;
+        AtomicInteger sign;
+        AtomicInteger val;
+
+        public Consumer(Object lock, AtomicInteger sign, AtomicInteger val) {
+            this.lock = lock;
+            this.sign = sign;
+            this.val = val;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    while (sign.get() == 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + val);
+                    sign.set(0);
+                    lock.notify();
+                }
+
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,82 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static int sign;
+    public static int val;
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        Producer producer = new Producer(lock, condition);
+        Consumer consumer = new Consumer(lock, condition);
+        sign = 0;
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        private ReentrantLock lock;
+        private Condition condition;
+
+        public Producer(ReentrantLock lock, Condition condition) {
+            this.lock = lock;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (ProducerConsumer2.sign != 0) {
+                        condition.await();
+                    }
+                    val = new Random().nextInt();
+                    System.out.println("Producing " + val);
+                    ProducerConsumer2.sign = 1;
+                    condition.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private ReentrantLock lock;
+        private Condition condition;
+
+        public Consumer(ReentrantLock lock, Condition condition) {
+            this.lock = lock;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (ProducerConsumer2.sign != 1) {
+                        condition.await();
+                    }
+                    System.out.println("Consuming " + val);
+                    ProducerConsumer2.sign = 0;
+                    condition.signal();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 }
