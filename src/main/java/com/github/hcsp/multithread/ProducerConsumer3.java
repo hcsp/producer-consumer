39926c35baf63626713da0ea1b1b486a3ed192a9
OF3832 <chengpengyeah@126.com>
2021-03-26 10:28:59 +0800

    ArrayBlockingQueue  实现一个生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,46 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+
+    private static ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(1);
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            int nextInt = new Random().nextInt();
+            try {
+                queue.put(nextInt);
+                System.out.println("Producing " + nextInt);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                Integer pro = queue.take();
+                System.out.println("Consuming " + pro);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
