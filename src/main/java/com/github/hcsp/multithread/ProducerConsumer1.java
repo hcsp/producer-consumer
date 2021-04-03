74a88d8ae0f377d9751a61dbfbd0a170e9fb8c62
cbx <2943189638@qq.com>
2021-04-03 19:32:19 +0800

    提交作业

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,12 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
+
+    private static Object lock = new Object();
+    private static Integer pro;
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -14,11 +20,43 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (pro != null) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int nextInt = new Random().nextInt();
+                    System.out.println("Producing " + nextInt);
+                    pro = nextInt;
+                    lock.notifyAll();
+                }
+            }
+        }
+
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (pro == null) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Consuming " + pro);
+                    pro = null;
+                    lock.notifyAll();
+                }
+            }
+        }
     }
 }
