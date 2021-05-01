c46f6bd02f913d176b150c69c25322b2e6cb3712
zhangxiaoxi3 <zhangxiaoxi3@jd.com>
2021-05-02 05:05:15 +0800

    ProducerConsumerTest

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,93 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+public class ProducerConsumer2 {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Container container = new Container();
+        Producer producer = new Producer(container);
+        Consumer consumer = new Consumer(container);
+        new Thread(producer).start();
+        Thread.sleep(100);
+        new Thread(consumer).start();
     }
 
-    public static class Consumer extends Thread {
+    public static class Producer implements Runnable {
+        Container container;
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                container.put();
+            }
+        }
+
+        public Producer(Container container) {
+            this.container = container;
+        }
+    }
+
+    public static class Consumer implements Runnable {
+        Container container;
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                container.take();
+            }
+        }
+
+        public Consumer(Container container) {
+            this.container = container;
+        }
+    }
+
+    public static class Container {
+        private Integer value;
+        private Lock lock;
+        private Condition consumer;
+        private Condition producer;
+
+        public void put() {
+            lock.lock();
+            while (null == value) {
+                int num = new Random().nextInt();
+                value = num;
+                System.out.println("Producing " + num);
+                consumer.signalAll();
+            }
+            try {
+                producer.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        public void take()  {
+            lock.lock();
+            while (null != value) {
+                Integer num = value;
+                System.out.println("Consuming " + num);
+                value = null;
+                producer.signalAll();
+            }
+            try {
+                consumer.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public Container() {
+            lock = new ReentrantLock();
+            consumer = lock.newCondition();
+            producer = lock.newCondition();
+        }
     }
 }
