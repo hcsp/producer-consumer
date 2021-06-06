b42c4aa1dd8915f959bf9c136368eb0bf2c83bc0
greek-zzf <1531037123@qq.com>
2021-06-06 23:13:45 +0800

    syschronize reentrantLock

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,24 +1,89 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+
+
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Object lock = new Object();
+        Container container = new Container();
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
 
-        producer.join();
-        producer.join();
     }
 
     public static class Producer extends Thread {
+        private Container container;
+        private Object lock;
+
+        public Producer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    int number = new Random().nextInt();
+                    System.out.println("生产:" + number);
+                    container.setValue(Optional.of(number));
+                    lock.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Container container;
+        private Object lock;
+
+        public Consumer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("消费:" + container.getValue().get());
+                    container.setValue(Optional.empty());
+                    lock.notify();
+                }
+            }
+        }
+    }
+
+    public static class Container {
+        private Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
