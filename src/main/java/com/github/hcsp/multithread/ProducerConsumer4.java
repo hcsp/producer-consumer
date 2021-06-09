e07822bf26917d5a4f5954ab415b912685a9aadb
Andrew Zhu <76999207+zeq96@users.noreply.github.com>
2021-06-09 09:44:58 +0800

    5种方式实现简单的生产-消费者模型 (#191)
    5种方式实现简单的生产-消费者模型 (#191)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,87 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Container container = new Container();
+        Semaphore semaphore = new Semaphore(10, true);
+        Producer producer = new Producer(semaphore, container);
+        Consumer consumer = new Consumer(semaphore, container);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        private Semaphore semaphore;
+        private Container container;
+
+        public Producer(Semaphore semaphore, Container container) {
+            this.semaphore = semaphore;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                while (container.getSharedData().isPresent()) {
+                    try {
+                        semaphore.acquire();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                int random = new Random().nextInt();
+                System.out.println("Producing " + random);
+                container.setSharedData(Optional.of(random));
+                semaphore.release();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Semaphore semaphore;
+        private Container container;
+
+        public Consumer(Semaphore semaphore, Container container) {
+            this.semaphore = semaphore;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                while (!container.getSharedData().isPresent()) {
+                    try {
+                        semaphore.acquire();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                int value = container.getSharedData().get();
+                System.out.println("Consuming " + value);
+                container.setSharedData(Optional.empty());
+                semaphore.release();
+            }
+        }
+    }
+
+    public static class Container {
+
+        private Optional<Integer> sharedData = Optional.empty();
+
+        public Optional<Integer> getSharedData() {
+            return sharedData;
+        }
+
+        public void setSharedData(Optional<Integer> sharedData) {
+            this.sharedData = sharedData;
+        }
     }
 }
