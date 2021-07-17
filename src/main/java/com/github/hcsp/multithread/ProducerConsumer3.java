b64a54ef30d8f00b733195654ac0717d6ce38a36
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-07-17 18:58:10 +0800

    optimize Object.wait + Object.notify & Condition & BlockingQueue (#194)
    optimize Object.wait + Object.notify & Condition & BlockingQueue (#194)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,68 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    private static final BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
+    private static final BlockingQueue<Integer> signalQueue = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            new CreateRunInstance().run(Type.PRODUCE);
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            new CreateRunInstance().run(Type.CONSUME);
+        }
     }
+
+
+    private static class CreateRunInstance implements CreateRun {
+        @Override
+        public void run(Enum<Type> type) {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    if (type == Type.CONSUME) {
+                        consume();
+                    } else {
+                        produce();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        @Override
+        public void produce() throws InterruptedException {
+            int random = new Random().nextInt();
+            System.out.println("Producing " + random);
+            queue.put(random);
+            signalQueue.take();
+        }
+
+        @Override
+        public void consume() throws InterruptedException {
+            System.out.println("Consuming " + queue.take());
+            signalQueue.put(0);
+        }
+    }
+
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,69 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.Exchanger;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    private static final Exchanger<Integer> exchanger = new Exchanger<Integer>();
+    private static final Basket basket = new Basket();
+    private static int index = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            new CreateRunInstance().run(Type.PRODUCE);
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            new CreateRunInstance().run(Type.CONSUME);
+        }
+    }
+
+
+    private static class CreateRunInstance implements CreateRun {
+        @Override
+        public void run(Enum<Type> type) {
+            while (index < 10) {
+                try {
+                    if (type == Type.CONSUME) {
+                        consume();
+                    } else {
+                        produce();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        @Override
+        public void produce() throws InterruptedException {
+            if (!basket.getValue().isPresent()) {
+                int random = Worker.Produce(basket);
+                exchanger.exchange(random);
+            }
+        }
+
+        @Override
+        public void consume() throws InterruptedException {
+            if (basket.getValue().isPresent()) {
+                Worker.Consume(basket);
+                index++;
+                exchanger.exchange(1);
+            }
+        }
     }
 }
