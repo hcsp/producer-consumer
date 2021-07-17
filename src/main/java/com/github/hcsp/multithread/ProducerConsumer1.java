bcfa3d3289870466b3bbdd21d19028b99f242107
bowen-wu <13793248162@163.com>
2021-07-17 14:21:09 +0800

    feat: use Object.wait() + Object.notify()

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    private static int index = 0;
+    private static final Object lock = new Object();
+    private static List<Integer> basket = new ArrayList<>(1);
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -14,11 +22,45 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            while (index < 10) {
+                synchronized (lock) {
+                    if (basket.isEmpty()) {
+                        int random = new Random().nextInt();
+                        basket.add(random);
+                        System.out.println("Producing " + basket.get(0));
+                        lock.notify();
+                    } else {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            while (index < 10) {
+                synchronized (lock) {
+                    if (basket.isEmpty()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        System.out.println("Consuming " + basket.get(0));
+                        basket.remove(0);
+                        index++;
+                        lock.notify();
+                    }
+                }
+            }
+        }
     }
 }
