19c1afca7643e5388b3f73c96dc3cf7c42673f95
yyzcl <yyzclyang@gmail.com>
2021-08-17 17:44:21 +0800

    finish task (#196)
    finish task (#196)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Container container = new Container();
+        Object lock = new Object();
+
+        Producer producer = new Producer(container, lock);
+        Consumer consumer = new Consumer(container, lock);
 
         producer.start();
         consumer.start();
@@ -13,12 +19,75 @@ public class ProducerConsumer1 {
     }
 
     public static class Producer extends Thread {
+        Container container;
+        final Object lock;
+
+        public Producer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int r = new Random().nextInt();
+                    container.setValue(Optional.of(r));
+                    System.out.println("Producing " + r);
+
+                    lock.notifyAll();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+        final Object lock;
+
+        public Consumer(Container container, Object lock) {
+            this.container = container;
+            this.lock = lock;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                synchronized (lock) {
+                    while (!container.getValue().isPresent()) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println("Consuming " + container.getValue().get());
+                    container.setValue(Optional.empty());
+
+                    lock.notifyAll();
+                }
+            }
+        }
+    }
+
+    public static class Container {
+        Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,89 @@
 package com.github.hcsp.multithread;
 
-public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
-    public static class Producer extends Thread {
-        @Override
-        public void run() {}
+public class ProducerConsumer4 {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(1);
+        Container container = new Container();
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+        Producer producer = new Producer(container, semaphore);
+        Consumer consumer = new Consumer(container, semaphore);
+
+        for (int i = 0; i < 10; i++) {
+            threadPool.execute(producer);
+            threadPool.execute(consumer);
+        }
+        threadPool.shutdown();
     }
 
-    public static class Consumer extends Thread {
+    public static class Producer implements Runnable {
+        Container container;
+        Semaphore semaphore;
+
+        public Producer(Container container, Semaphore semaphore) {
+            this.container = container;
+            this.semaphore = semaphore;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                while (!container.getValue().isPresent()) {
+                    semaphore.acquire();
+                    int r = new Random().nextInt();
+                    container.setValue(Optional.of(r));
+                    System.out.println("Producing " + r);
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }
+    }
+
+    public static class Consumer implements Runnable {
+        Container container;
+        Semaphore semaphore;
+
+        public Consumer(Container container, Semaphore semaphore) {
+            this.container = container;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (container.getValue().isPresent()) {
+                    semaphore.acquire();
+                    System.out.println("Consuming " + container.getValue().get());
+                    container.setValue(Optional.empty());
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }
+    }
+
+    public static class Container {
+        Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
