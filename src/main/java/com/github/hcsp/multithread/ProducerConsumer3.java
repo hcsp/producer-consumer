3312fbd2a3f428c9342730fbf7a9d708008545be
zdmhahaha <987854018@qq.com>
2021-11-29 14:18:24 +0800

    实现一个生产者-消费者模型

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,73 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+
+        BlockingDeque blockingDeque = new LinkedBlockingDeque(1);
+        BlockingDeque blockingDeque0 = new LinkedBlockingDeque(1);
+
+        Producer producer = new Producer(blockingDeque,blockingDeque0);
+        Consumer consumer = new Consumer(blockingDeque,blockingDeque0);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        BlockingDeque<Integer> blockingDeque;
+        BlockingDeque<Integer> blockingDeque0;
+
+        public Producer(BlockingDeque<Integer> blockingDeque,BlockingDeque<Integer> blockingDeque0) {
+            this.blockingDeque = blockingDeque;
+            this.blockingDeque0 = blockingDeque0;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                try {
+                    blockingDeque.put(r);
+                    //停在这里
+                    blockingDeque0.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        BlockingDeque<Integer> blockingDeque;
+        BlockingDeque<Integer> blockingDeque0;
+
+        public Consumer(BlockingDeque<Integer> blockingDeque,BlockingDeque<Integer> blockingDeque0) {
+            this.blockingDeque = blockingDeque;
+            this.blockingDeque0 = blockingDeque0;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    //停在这里
+                    Integer a = blockingDeque.take();
+                    System.out.println("Consuming " + a);
+                    blockingDeque0.put(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,88 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+        public static void main(String[] args) throws InterruptedException {
+
+            Container container = new Container();
+
+            Producer producer = new Producer(container);
+            Consumer consumer = new Consumer(container);
+
+            producer.start();
+            consumer.start();
+
+            producer.join();
+            producer.join();
+        }
 
     public static class Producer extends Thread {
+        Container container;
+
+        public Producer(Container container) {
+            this.container = container;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int j = 0; j < 10; j++) {
+                try {
+                    container.semaphore.acquire();
+                    while (!container.getOptional().isPresent()) {
+                        int r = new Random().nextInt();
+                        System.out.println("Producing " + r);
+                        container.setOptional(Optional.of(r));
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    container.semaphore0.release();
+                }
+
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        Container container;
+
+        public Consumer(Container container) {
+            this.container = container;
+        }
         @Override
-        public void run() {}
+        public void run() {
+            for (int j = 0; j < 10; j++) {
+                try {
+
+                    while (!container.getOptional().isPresent()){
+                        container.semaphore0.acquire();
+                    }
+                    Integer a = container.getOptional().get();
+                    System.out.println("Consuming " + a);
+                    container.setOptional(Optional.empty());
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    container.semaphore.release();
+                }
+            }
+        }
+    }
+    static class Container {
+        private Optional<Integer> optional = Optional.empty();
+        Semaphore semaphore =new Semaphore(1);
+        Semaphore semaphore0 =new Semaphore(0);
+
+
+        public Optional<Integer> getOptional() {
+            return optional;
+        }
+
+        public void setOptional(Optional<Integer> optional) {
+            this.optional = optional;
+        }
     }
 }
