cbf066331c11a63a694009bdd5f9772b5795ae90
Nicklzy <tydx11@hotmail.com>
2022-01-11 22:32:16 +0800

    消费者模型 (#202)
    消费者模型 (#202)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,113 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Container container = new Container(lock);
+        Producer producer = new Producer(lock, container);
+        Consumer consumer = new Consumer(lock, container);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        public Producer(ReentrantLock lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
+        private final ReentrantLock lock;
+        private final Container container;
+
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (this.container.getValue().isPresent()) {
+                        try {
+                            container.getNotConsumed().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Integer value = new Random().nextInt();
+                    this.container.setValue(Optional.of(value));
+                    System.out.println("produce " + value);
+                    this.container.getNotProduced().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private final ReentrantLock lock;
+        private final Container container;
+
+        public Consumer(ReentrantLock lock, Container container) {
+            this.lock = lock;
+            this.container = container;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                lock.lock();
+                try {
+                    while (!this.container.getValue().isPresent()) {
+                        try {
+                            container.getNotProduced().await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("consume " + this.container.getValue().get());
+                    this.container.setValue(Optional.empty());
+                    this.container.getNotConsumed().signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    private static class Container {
+        Container(ReentrantLock lock) {
+            this.notConsumed = lock.newCondition();
+            this.notProduced = lock.newCondition();
+        }
+
+        public Condition getNotConsumed() {
+            return notConsumed;
+        }
+
+        public Condition getNotProduced() {
+            return notProduced;
+        }
+
+        private final Condition notConsumed;
+        private final Condition notProduced;
+        private Optional<Integer> value = Optional.empty();
+
+        public Optional<Integer> getValue() {
+            return value;
+        }
+
+        public void setValue(Optional<Integer> value) {
+            this.value = value;
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer3.java
@@ -1,24 +1,67 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ProducerConsumer3 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+        BlockingQueue<Integer> lockQueue = new LinkedBlockingQueue<>(1);
+        Producer producer = new Producer(queue, lockQueue);
+        Consumer consumer = new Consumer(queue, lockQueue);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> lockQueue;
+
+        public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> lockQueue) {
+            this.queue = queue;
+            this.lockQueue = lockQueue;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                Integer value = new Random().nextInt();
+                try {
+                    this.queue.put(value);
+                    System.out.println("produce " + value);
+                    this.lockQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        public Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> lockQueue) {
+            this.queue = queue;
+            this.lockQueue = lockQueue;
+        }
+
+        BlockingQueue<Integer> queue;
+        BlockingQueue<Integer> lockQueue;
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Object value = this.queue.take();
+                    System.out.println("consume " + value);
+                    this.lockQueue.put(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
