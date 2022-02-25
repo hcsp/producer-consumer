470d22d312838b2df2222fcf6785a99e2de26365
soohea <77014206+soohea@users.noreply.github.com>
2022-02-25 15:42:29 +0800

    synchronized&lock&BlockingQueue&semaphore&exchanger (#203)
    synchronized&lock&BlockingQueue&semaphore&exchanger (#203)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,57 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static Exchanger<Integer> exchanger = new Exchanger<>();
+    static Exchanger<Integer> exchanger1 = new Exchanger<>();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                int random = new Random().nextInt();
+                System.out.println("Producing " + random);
+                try {
+                    exchanger.exchange(random);
+                    exchanger1.exchange(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int random = exchanger.exchange(null);
+                    System.out.println("Consuming " + random);
+                    exchanger1.exchange(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
+
+
