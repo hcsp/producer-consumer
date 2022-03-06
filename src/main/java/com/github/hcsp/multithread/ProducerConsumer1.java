2de1ea6593baa289f9997598bc5ca7b7bbed5b6e
赵童 <freetie@qq.com>
2022-03-06 22:53:57 +0800

    finish one class (#204)
    finish one class (#204)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ProducerConsumer1 {
+    static LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1);
+    static LinkedBlockingQueue<Integer> signalQueue = new LinkedBlockingQueue<>(1);
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -14,11 +19,31 @@ public class ProducerConsumer1 {
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    int randomNumber = new Random().nextInt();
+                    queue.put(randomNumber);
+                    System.out.println("Producing " + randomNumber);
+                    signalQueue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Consuming " + queue.take());
+                    signalQueue.put(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
