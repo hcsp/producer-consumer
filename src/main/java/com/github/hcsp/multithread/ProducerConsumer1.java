e4475b16ca07fe2c87364123d8a42fdab3634de3
magicalmuggle <magicalmuggle@foxmail.com>
2022-04-20 14:25:15 +0800

    Object.wait/notify && Lock/Condition && BlockingQueue && Semaphore && Exchanger

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,6 +1,14 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+// Object.wait / notify
 public class ProducerConsumer1 {
+    static Queue<Integer> queue = new LinkedList<>();
+    static final Object LOCK = new Object();
+
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
         Consumer consumer = new Consumer();
@@ -9,16 +17,48 @@ public class ProducerConsumer1 {
         consumer.start();
 
         producer.join();
-        producer.join();
+        consumer.join();
     }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (LOCK) {
+                Random random = new Random();
+                for (int i = 0; i < 10; i++) {
+                    while (!queue.isEmpty()) {
+                        safeWait();
+                    }
+                    int intProduced = random.nextInt();
+                    queue.offer(intProduced);
+                    System.out.println("Producing " + intProduced);
+                    LOCK.notify();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (LOCK) {
+                for (int i = 0; i < 10; i++) {
+                    while (queue.isEmpty()) {
+                        safeWait();
+                    }
+                    Integer intConsumed = queue.poll();
+                    System.out.println("Consuming " + intConsumed);
+                    LOCK.notify();
+                }
+            }
+        }
+    }
+
+    private static void safeWait() {
+        try {
+            LOCK.wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer5.java
@@ -1,24 +1,51 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
+// Exchanger
 public class ProducerConsumer5 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static Exchanger<Integer> exchanger = new Exchanger<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                Random random = new Random();
+                int intProduced = random.nextInt();
+                safeExchange(intProduced);
+                System.out.println("Producing " + intProduced);
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                Integer intConsumed = safeExchange(0);
+                System.out.println("Consuming " + intConsumed);
+            }
+        }
+    }
+
+    private static Integer safeExchange(int intProduced) {
+        try {
+            return exchanger.exchange(intProduced);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
