e58fa24d150183819c57fad770e8067c0c75ce73
bowen-wu <13793248162@163.com>
2022-07-22 17:18:05 +0800

    completed ProducerConsumer

---
+++ src/main/java/com/github/hcsp/multithread/Container.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.multithread;
+
+public class Container {
+    Object value;
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
