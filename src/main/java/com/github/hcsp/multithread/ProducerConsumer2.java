03f1d5d705bd84ec78885c26e3ff1a4ca8ebfaff
ang tsang <tsang1205@qq.com>
2022-08-28 15:29:12 +0800

    feat: three methods to do producer-consumer (#211)
    feat: three methods to do producer-consumer (#211)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,81 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        ProducerConsumer1.Container container = new ProducerConsumer1.Container();
+
+        Producer producer = new Producer(lock, condition, container);
+        Consumer consumer = new Consumer(lock, condition, container);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
 
     public static class Producer extends Thread {
+        final ReentrantLock lock;
+        final Condition condition;
+        final ProducerConsumer1.Container container;
+
+        Producer(ReentrantLock lock, Condition condition, ProducerConsumer1.Container container) {
+            this.lock = lock;
+            this.container = container;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            lock.lock();
+            try {
+                while (container.getContainer().isPresent()) {
+                    condition.await();
+                }
+                Integer num = new Random().nextInt();
+                System.out.println("Producing " + num);
+                container.setContainer(Optional.of(num));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                condition.signal();
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
+        final ReentrantLock lock;
+        final Condition condition;
+        final ProducerConsumer1.Container container;
+
+        Consumer(ReentrantLock lock, Condition condition, ProducerConsumer1.Container container) {
+            this.lock = lock;
+            this.container = container;
+            this.condition = condition;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            lock.lock();
+            try {
+                while (!container.getContainer().isPresent()) {
+                    condition.await();
+                }
+                System.out.println("Consuming " + container.getContainer().get());
+                container.setContainer(Optional.empty());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                condition.signal();
+            }
+        }
     }
 }
