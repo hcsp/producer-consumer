1f27a1e738560fad1e908c8db75924bfae95240c
Leo Cheung <leozhang621@gmail.com>
2024-01-18 13:09:14 +0800

    implement ProducerConsumer using 3 different methods (#214)
    implement ProducerConsumer using 3 different methods (#214)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
         Producer producer = new Producer();
@@ -12,13 +14,54 @@ public class ProducerConsumer1 {
         producer.join();
     }
 
+    private static final Object LOCK = new Object();
+    private static final int MAX_CONSUME_COUNT = 10;
+    private static Integer randomInt;
+    private static int consumeCount = 0;
+
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (LOCK) {
+                try {
+                    while (true) {
+                        if (randomInt != null) {
+                            System.out.println("Consuming " + randomInt);
+                            randomInt = null;
+                            consumeCount += 1;
+                            if (consumeCount == MAX_CONSUME_COUNT) {
+                                break;
+                            }
+                        }
+                        LOCK.notify();
+                        LOCK.wait();
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (LOCK) {
+                try {
+                    while (true) {
+                        randomInt = new Random().nextInt();
+                        System.out.println("Producing " + randomInt);
+                        LOCK.notify();
+                        if (consumeCount == MAX_CONSUME_COUNT - 1) {
+                            break;
+                        } else {
+                            LOCK.wait();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,55 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        producer.join();
+    }
+
+    private static final BlockingQueue<Integer> q = new ArrayBlockingQueue<>(1);
+    private static final BlockingQueue<Integer> notifyQ = new ArrayBlockingQueue<>(1);
+    private static final int MAX_CONSUME_COUNT = 10;
+    private static int consumeCount = 0;
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                while (consumeCount != MAX_CONSUME_COUNT) {
+                    int randomInt = new Random().nextInt();
+                    System.out.println("Producing " + randomInt);
+                    q.put(randomInt);
+                    notifyQ.take();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                do {
+                    int randomInt = q.take();
+                    System.out.println("Consuming " + randomInt);
+                    consumeCount += 1;
+                    notifyQ.put(1);
+                } while (consumeCount != MAX_CONSUME_COUNT);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
