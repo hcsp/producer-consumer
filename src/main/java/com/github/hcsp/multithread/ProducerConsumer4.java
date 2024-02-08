13a703222ccdb42640f3d7f931cbb187018d7153
rf4github <39894049+rf818@users.noreply.github.com>
2024-02-08 15:14:50 +0800

    producer-consumer (#215)
    producer-consumer (#215)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer4.java
@@ -1,24 +1,47 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer4 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    static final Semaphore toConsume = new Semaphore(0);
+    static final Semaphore toProduce = new Semaphore(1);
+    static int product = 0;
+
+    static final Object plate = new Object();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            new Producer().start();
+            new Consumer().start();
+        }
+    }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                toProduce.acquire();
+                product = new Random().nextInt();
+                System.out.println("Producing " + product);
+                toConsume.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            try {
+                toConsume.acquire();
+                System.out.println("Consuming " + product);
+                toProduce.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
