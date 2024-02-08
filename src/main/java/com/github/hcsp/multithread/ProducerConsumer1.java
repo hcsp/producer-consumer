e598fcf3380106089fa97db124745e9aab729275
dexter_git <runfenglin@qq.com>
2024-02-08 15:13:50 +0800

    producer-consumer

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,24 +1,54 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
 public class ProducerConsumer1 {
+    //wait/notify
+    static final Object plate = new Object();
+    static boolean hasContent = Boolean.FALSE;
+    static int product;
+
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
-
-        producer.start();
-        consumer.start();
-
-        producer.join();
-        producer.join();
+        for (int i = 0; i < 10; i++) {
+            new Producer().start();
+            new Consumer().start();
+        }
     }
 
     public static class Producer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (plate) {
+                while (hasContent) {
+                    try {
+                        plate.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                product = new Random().nextInt();
+                System.out.println("Producing " + product);
+                hasContent = true;
+                plate.notifyAll();
+            }
+        }
     }
 
     public static class Consumer extends Thread {
         @Override
-        public void run() {}
+        public void run() {
+            synchronized (plate) {
+                while (!hasContent) {
+                    try {
+                        plate.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println("Consuming " + product);
+                hasContent = false;
+                plate.notifyAll();
+            }
+        }
     }
 }
